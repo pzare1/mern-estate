54d82d8cb8e02079f23c15d50dde88b25c548282
Pouya <zarepouya7@gmail.com>
2023-11-05 23:05:50 +0200

    Add routes and pages to client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+BrowserRouter
 function App() {
   return (
-    <div className=' bg-red-700'>App</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/login' element={<SignIn/>}/>
+          <Route path='/register' element={<SignOut/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
