c39478f8dc0f4b0f3c1d5ac7b468239485f452c4
Pouya <zarepouya7@gmail.com>
2023-11-06 23:38:23 +0200

    Add header component to the project

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1 className='font-bold text-sm sm:text-lg flex flex-wrap'>
+            <span className=' text-slate-500'>Real</span>
+            <span className=' text-slate-700'>Estate</span>
+        </h1>
+    </header>
+  )
+}
+
+export default Header
