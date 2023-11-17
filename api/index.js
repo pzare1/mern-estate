c02967c0dd15ae2499469e30b0b1fa576eb293a4
Pouya <zarepouya7@gmail.com>
2023-11-18 01:44:51 +0200

    Add the auth config to the project

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './Routes/user.route.js'
+import userRouter from './Routes/user.route.js';
+import authRouter from './Routes/auth.route.js'
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose.connect(process.env.MONGO)
     console.log(err)
 });
 
+app.use(express.json());    
+
 app.listen(PORT,() => {
     console.log('server is running');
 })
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter);
