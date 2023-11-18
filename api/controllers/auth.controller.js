3ce9f56b4978174015c66705b220f52894ef04eb
Pouya <zarepouya7@gmail.com>
2023-11-18 02:09:01 +0200

    add middleware for project and add a error file

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';     
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signupController = async(req, res) => {
+export const signupController = async(req, res, next) => {
     const {username , email , password } = req.body;  
     const hashedpassword = await bcryptjs.hashSync(password, 10);     
     const newUser = new User({ username, email, password: hashedpassword });    
@@ -9,7 +10,7 @@ export const signupController = async(req, res) => {
         await newUser.save()
         res.status(201).json({ message: 'User created successfully' }); 
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error' });
+        next(error);
     }
 
 }
