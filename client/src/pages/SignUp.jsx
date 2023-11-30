dfd28e32240995195a2386a1f125142e7397959e
Pouya <zarepouya7@gmail.com>
2023-11-30 19:26:26 +0200

    Complete the registration part

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from 'react'
+import { useNavigate } from'react-router-dom'   
 
 function SignUp() {
     const [formData, setformData] = useState({});
+    const [errors, setErrors] = useState(null); 
+    const [loading, setloading] = useState(false)
+    const navigate = useNavigate();
     const changeHandle = async(e) => {
         setformData({...formData, [e.target.id]: e.target.value});
     }
     const submitHandle = async (e) => {
         e.preventDefault();
-        const res = await fetch('/api/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-        const data = await res.json();
-        console.log(data);
-    }      
-    console.log(formData);  
+        try {
+            setloading(true);   
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+            if(data.success === false){
+                setErrors(data.message);
+                setloading(false);
+                return;
+            }
+            setloading(false);
+            setErrors(null); 
+            navigate('/login')
+        } catch (error) {
+            setloading(false);
+            setErrors(error.message);   
+        }
+    };      
   return (
     <div>
         <h1 className='my-5 sm:my-7 text-center font-semibold sm:text-xl text-sm text-slate-400 bg-slate-200 w-full max-w-md mx-auto py-3 rounded-md'>
@@ -32,9 +48,10 @@ function SignUp() {
                     <input className=' my-2 placeholder:text-sm shadow appearance-none border rounded w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline' id='email' type='email' placeholder='Email' onChange={changeHandle}/>
                     <label className='block text-slate-600 text-sm font-bold mb-2' htmlFor='password'></label>
                     <input className=' my-2 placeholder:text-sm shadow appearance-none border rounded w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline' id='password' type='password' placeholder='Password' onChange={changeHandle}/>
-                    <button className='my-2 w-full bg-slate-600 hover:bg-slate-500 text-slate-200 text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign Up</button>
+                    <button disabled={loading} className='my-2 w-full bg-slate-600 hover:bg-slate-500 text-slate-200 text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline'>{loading ? 'Loading' : 'Register'}</button>
                     <button className='my-2 w-full bg-green-600 hover:bg-green-500 text-green-50 text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Login</button>
                 </div>
+                {errors && <div className='text-red-600 text-sm p-2 bg-red-100 rounded-md my-2'>{errors}</div>}
             </div>
         </form>
     </div>
