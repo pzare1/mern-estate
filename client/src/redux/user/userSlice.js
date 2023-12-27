cdcc73ba20511d042d5ecfee4fbe1a65705ff332
Pouya <zarepouya7@gmail.com>
2023-12-28 01:09:42 +0200

    Add redux toolkit to the project

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'; 
+
+const initialState = {
+    currentUser: null,
+    loading: false, 
+    error: null,    
+}
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        signinStart: (state) => {
+            state.loading = true;
+        },
+        signinSuccess: (state, action) => {
+            state.loading = false;
+            state.currentUser = action.payload;
+            state.error = null;
+        },
+        signinFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload; 
+        }
+    }
+});
+
+export const { signinStart, signinSuccess, signinFailure } = userSlice.actions; 
+
+export default userSlice.reducer;   
